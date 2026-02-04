1e574c6fa0edf09242ba22995666196a8f2c984a
Grzegorz Jasek <grzegorz.jasek@comp-ci.pl>
2026-02-04 12:21:38 +0100

    add auto login with token

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,36 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { useRouter, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import 'react-native-reanimated';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function RootLayout() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem('jwt');
+        if (token) {
+          setTimeout(() => router.replace("/(main)/home"), 10);
+        }
+      } catch(e) {
+        console.log("Błąd odczytu tokena", e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    checkToken();
+  }, []);
+
   const colorScheme = useColorScheme();
 
   return (
     <SafeAreaProvider>
-      {/* Poprawiony warunek: 'dark' zamiast '#FFF' */}
       <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(auth)/login" />
